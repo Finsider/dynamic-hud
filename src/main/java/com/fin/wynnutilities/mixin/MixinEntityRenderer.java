0c69cc0d24fb55b513bf61cee4d089f4388b9074
Finsider <muhammadazzar56@gmail.com>
2024-09-08 15:26:31 +0700

    namnetags!!!!

---
+++ src/main/java/com/fin/wynnutilities/mixin/MixinEntityRenderer.java
@@ -0,0 +1,20 @@
+package com.fin.wynnutilities.mixin;
+
+import com.fin.wynnutilities.Main;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityRenderer.class)
+public class MixinEntityRenderer {
+    @Inject(at = @At("HEAD"), method = "render", cancellable = true)
+    private void deleteNametags(Entity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        if (Main.settings.extraSettings.deletePlayerNametags && entity instanceof PlayerEntity) ci.cancel();
+    }
+}
